9d7edbb1a69e942711096502bc3dbcff0dacf7ca
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-11-15 14:32:07 -0800

    Touch up docs for contains? slightly more

--- src/thalia/doc.clj
+++ src/thalia/doc.clj
@@ -278,11 +278,12 @@ The intValue conversion can lead to unexpected behavior, demonstrated
 below.  This happens because intValue converts the long and double
 values shown to 0, which is in the range [0,2] of indices.
 
-    user=> (contains? \"abc\" (+ 4 (* 4 Integer/MAX_VALUE)))
+    user=> (def long-truncates-to-int-0 (bit-shift-left 1 33))
+    user=> (contains? \"abc\" long-truncates-to-int-0)
     true
     user=> (contains? \"abc\" -0.99)
     true
-    user=> (contains? [:a :b :c] (+ 4 (* 4 Integer/MAX_VALUE)))
+    user=> (contains? [:a :b :c] long-truncates-to-int-0)
     true
     user=> (contains? [:a :b :c] 0.5)
     false       ; only integer values can return true for vectors
