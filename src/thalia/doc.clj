68ccc6cb9c9c9251dc044a2817f7096aa55a6a13
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-11-18 09:31:11 -0800

    Fix bug in append-doc!, separate out do-the-add! from add-extra-docs!
    and make do-the-add! error messages more informative.  Also add :debug
    true option to add-extra-docs! to see what is happening when things
    are going well.

--- src/thalia/doc.clj
+++ src/thalia/doc.clj
@@ -1,13 +1,14 @@
 (ns thalia.doc
   (:require [clojure.pprint :as pp]
             [clojure.java.io :as io]
+            [clojure.set :as set]
             [thalia.utils :refer [iprintf read-safely]]))
 
 
 (defn- append-doc! [v additional-docstring]
   (let [m (meta v)
         ;; Save the original doc string if we have not done so earlier
-        m (if (:thalia/original-doc meta)
+        m (if (contains? m :thalia/original-doc)
             m
             (assoc m :thalia/original-doc (:doc m)))
         ^String orig-doc (:thalia/original-doc m)
@@ -35,6 +36,36 @@
         nil)))
 
 
+(defn- do-the-add! [opts rsrc-name doc-data]
+  ;; TBD: Is there a 'standard' way to check for libraries other
+  ;; than clojure.core what version is loaded?
+  (doseq [[{:keys [library version]} data-for-symbols] doc-data]
+    (doseq [one-sym-data data-for-symbols]
+      (if-let [missing-keys (seq (set/difference #{:ns :symbol
+                                                   :extended-docstring}
+                                                 (set (keys one-sym-data))))]
+        (do
+          (binding [*out* *err*]
+            (iprintf "Resource file '%s' has the following map that is missing needed keys %s:\n"
+                     rsrc-name missing-keys)
+            (pp/pprint one-sym-data)
+            (println)))
+        (let [ns (:ns one-sym-data)
+              sym-name-str (:symbol one-sym-data)
+              extended-docstring (:extended-docstring one-sym-data)
+              sym (symbol ns sym-name-str)]
+          (if-let [var (try (resolve sym)
+                            (catch Exception e nil))]
+            (do
+              (append-doc! var extended-docstring)
+              (when (:debug opts)
+                (iprintf *err* "Added extended doc string for symbol %s\n" sym)))
+            (iprintf *err* "No such var %s/%s
+    -- do you have a different version of library '%s' than '%s'?
+"
+                     ns sym-name-str library version)))))))
+
+
 (defn add-extra-docs! [& args]
   (let [default-locale (str (java.util.Locale/getDefault))
         opts (merge {:language default-locale}
@@ -46,24 +77,7 @@
 or no one has written thalia docs in that language yet.\n"
                rsrc-name (:language opts))
       (let [doc-data (read-safely file)]
-        ;; TBD: Is there a 'standard' way to check for libraries other
-        ;; than clojure.core what version is loaded?
-        (doseq [[{:keys [library version]} data-for-symbols] doc-data]
-          (doseq [one-sym-data data-for-symbols]
-            (let [ns (:ns one-sym-data)
-                  sym (:symbol one-sym-data)
-                  extended-docstring (:extended-docstring one-sym-data)]
-              (if (and ns sym extended-docstring)
-                (if-let [var (try (resolve (symbol ns sym))
-                                  (catch Exception e nil))]
-                  (append-doc! var extended-docstring)
-                  (iprintf *err* "No such var %s/%s\n" ns sym))
-                ;; else
-                (do
-                  (binding [*out* *err*]
-                    (iprintf "Resource file has this record that is missing one of the keys [:ns :symbol :extended-docstring]:\n")
-                    (pp/pprint one-sym-data))
-                  )))))))))
+        (do-the-add! opts rsrc-name doc-data)))))
 
 
 (comment
